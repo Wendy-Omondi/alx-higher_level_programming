3938d8604d14f697283a0da5b54650a96d1c6fe5
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-12-15 20:35:36 +0000

    find the max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        biggest = my_list[0]
+        for i in my_list[1:]:
+            if i > result:
+                biggest = i
+        return biggest
