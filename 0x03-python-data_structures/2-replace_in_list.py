ddae16423fcfc298acac0881b98de1243437d324
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-12-15 15:56:52 +0000

    shorten

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,6 +4,6 @@ def replace_in_list(my_list, idx, element):
         return my_list
     if 0 <= idx < len(my_list):
         return my_list
-    elif idx >= 0 and idx < len(my_list):
+    else:
         my_list[idx] = element
         return my_list
