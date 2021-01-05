68423154e5b696e97d8f833465183fb6839620b2
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-01-05 20:34:03 +0000

    added coments

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""a class Square that defines a square"""
+
+
 class Square:
+    """A class named Square
+    attribute1 (size): size of square
+    """
     def __init__(self, size):
+        """Initializes a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
         self.__size = size
