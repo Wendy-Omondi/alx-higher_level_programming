9e73346254d0ef55607589f11ab39623101747db
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-01-05 20:38:44 +0000

    added comments

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
+"""a class Square that defines a square"""
+
+
 class Square:
+    """A class named Square
+    attribute1 (size): size of square
+    """
+
     def __init__(self, size=0):
+        """Initializes a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
+
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
