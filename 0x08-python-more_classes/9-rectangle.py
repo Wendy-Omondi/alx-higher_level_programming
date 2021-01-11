7236fbf430813129cb93159286ce849f3ef32ad1
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-01-11 20:18:07 +0000

    a square is a rectangle

---
+++ 0x08-python-more_classes/9-rectangle.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python3
+"""a class rectangle that defines a rectangle"""
+
+
+class Rectangle:
+    """A class named Rectangle
+       with height, width, number of instances
+       and symbol
+    """
+
+    number_of_instances = 0
+    print_symbol = '#'
+
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+        type(self).number_of_instances += 1
+
+    @property
+    def height(self):
+        """retrieves height of rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """sets height of the rectangle"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def width(self):
+        """retrieves width of rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """sets width of the rectangle"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    def area(self):
+        """returns the rectangle area"""
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """returns the rectangle's perimeter"""
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return (self.__height * 2) + (self.__width * 2)
+
+    def __str__(self):
+        """Returns the string representation of the class instance"""
+        hashstring = ""
+        if self.__width == 0 or self.__height == 0:
+            return hashstring
+        for row in range(self.__height):
+            for column in range(self.__width):
+                hashstring += str(self.print_symbol)
+            if row < self.__height - 1:
+                hashstring += "\n"
+        return hashstring
+
+    def __repr__(self):
+        """recreates a new instance"""
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+
+    def __del__(self):
+        """prints detection message when instance is deleted"""
+        type(self).number_of_instances -= 1
+        print("Bye rectangle...")
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """returns bigger rectangle"""
+        if type(rect_1) != Rectangle:
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if type(rect_2) != Rectangle:
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_2.area() > rect_1.area():
+            return rect_2
+        else:
+            return rect_1
+
+    @classmethod
+    def square(cls, size=0):
+        return cls(size, size)
