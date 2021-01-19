af14706cbfeeee944ffaf112549e104f0e7b331e
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-01-19 20:10:24 +0000

    text

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,11 +2,11 @@
 """This module defines the number_of_lines function"""
 
 
-def number_of_lines(filename=""):
+def write_file(filename="", text=""):
     """Returns the number of lines of a text file
     """
     i = 0
     with open(filename, encoding='utf-8') as file:
-        for line in file:
+        for text in file:
             i += 1
         return i
