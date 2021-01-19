a73b49a06c70ebcbc2a5a410124a8f1348227fce
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-01-19 20:13:05 +0000

    append

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,14 +2,14 @@
 """This module defines the read_lines function"""
 
 
-def read_lines(filename="", nb_lines=0):
+def read_lines(filename="", text=""):
     """appends a string at the end of a text file (UTF8)
        and returns the number of characters added
     """
     with open(filename, encoding='utf-8') as file:
-        if nb_lines <= 0:
+        if text <= 0:
             for line in file:
                 print(line, end="")
         else:
-            for line in range(nb_lines):
+            for line in range(text):
                 print(file.readline(), end="")
