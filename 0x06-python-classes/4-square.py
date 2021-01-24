25ec757cf2de1ce93e44a213076bfed7b86a8f48
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-01-24 15:54:28 +0000

    size fix

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -8,6 +8,13 @@ class Square:
     """
 
     def __init__(self, size=0):
+        """Initializes a new Square.
+           Args:
+           size (int): The size of the new square.
+        """
+
+        self.__size = size
+
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
