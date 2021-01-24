8a4e5b314685d54c6e288b2e4a50ade90c38c002
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-01-24 15:51:06 +0000

    add size

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -7,6 +7,13 @@ class Square:
     attribute1 (size): size of square
     """
     def __init__(self, size=0):
+        """Initializes a new Square.
+           Args:
+           size (int): The size of the new square.
+        """
+
+        self.__size = size
+
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
