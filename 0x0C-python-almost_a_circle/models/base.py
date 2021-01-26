963ab3a70187e76117afd43c3f6d80354beebcb6
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-01-26 19:00:38 +0000

    base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Defines Base class"""
+
+
+import os
+import json
+
+
+class Base:
+
+    """Base class"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Instantiation function for Base instance
+        """
+
+        if id is not None:
+            self.id = id
+
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
