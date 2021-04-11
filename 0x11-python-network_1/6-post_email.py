f097ea0b293563aab232dd9f5d5eee2a7cf82796
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-04-11 19:46:56 +0000

    POST an email

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+   takes in a URL and an email address, sends a POST request to the passed URL
+   with the email as a parameter, and finally displays the body of the response.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    url_param = {'email': email}
+
+    req = requests.post(url, data=url_param)
+    print(req.text)
