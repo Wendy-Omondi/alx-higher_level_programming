e3bd79a667e65176b9729ccc3f976506b3d896c2
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-04-16 14:09:11 +0000

    class to JSON

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-"""This module creates an Object from a JSON file"""
+"""Defines a Python class-to-JSON function."""
 
 
-import json
-
-
-def load_from_json_file(filename):
-    """returns the dictionary description with simple data structure
-       (list, dictionary, string, integer and boolean)
-       for JSON serialization of an object
-    """
-    with open(filename, encoding='utf-8') as file:
-        return json.loads(file.read())
+def class_to_json(obj):
+    """Return the dictionary represntation of a simple data structure."""
+    return obj.__dict__
