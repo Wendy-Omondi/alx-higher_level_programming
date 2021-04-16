4dd0bc556b6e80c285243a065084430fda8ba6e7
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-04-16 12:09:05 +0000

    raises an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines class named BaseGeometry"""
+
+
+class BaseGeometry:
+    """An class named BaseGeometry
+    """
+    def area(self):
+        """raises an exception"""
+        raise Exception("area() is not implemented")
