c59b625e66f029ed9b34dcf019715e7b00e139e8
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-04-16 13:55:45 +0000

    load , add , save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-"""This module defines the save_to_json_file function"""
+"""adds argumentss to Python list and saves them"""
 
+from os import path
+from sys import argv
 
-import json
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-
-def save_to_json_file(my_obj, filename):
-    """a script that adds all arguments to a Python list,
-       and then save them to a file
-    """
-    with open(filename, 'w', encoding='utf-8') as file:
-        file.write(json.dumps(my_obj))
+if path.exists("add_item.json") is False:
+    save_to_json_file([], "add_item.json")
+my_list = load_from_json_file("add_item.json")
+for i in range(1, len(argv)):
+    my_list.append(argv[i])
+save_to_json_file(my_list, "add_item.json")
