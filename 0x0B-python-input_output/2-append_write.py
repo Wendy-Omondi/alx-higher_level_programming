2a74507d10f34000d56dce9f8c2f574404e866ab
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-04-16 13:38:41 +0000

    appends a string to the end ogf the text file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
-"""This module defines the read_lines function"""
+"""Defines the append_write function"""
 
 
-def read_lines(filename="", text=""):
-    """appends a string at the end of a text file (UTF8)
-       and returns the number of characters added
+def append_write(filename="", text=""):
+    """Appends a string at end of a text file, returns # of characters written
     """
-    with open(filename, encoding='utf-8') as file:
-        if text <= 0:
-            for line in file:
-                print(line, end="")
-        else:
-            for line in range(text):
-                print(file.readline(), end="")
+    with open(filename, 'a', encoding='utf-8') as file:
+        return file.write(text)
