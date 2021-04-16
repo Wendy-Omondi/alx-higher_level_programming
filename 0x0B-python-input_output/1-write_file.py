f6f50cdc7c82c3cea97826800aca1e0fe47a2e3c
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-04-16 13:32:40 +0000

    write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-"""This module defines the number_of_lines function"""
+"""Defines the write_file function"""
 
 
 def write_file(filename="", text=""):
-    """Returns the number of lines of a text file
-    """
-    i = 0
-    with open(filename, encoding='utf-8') as file:
-        for text in file:
-            i += 1
-        return i
+    """Writes a string to a text file and returns number of characters written"""
+    with open(filename, 'w', encoding='utf-8') as file:
+        return file.write(text)
