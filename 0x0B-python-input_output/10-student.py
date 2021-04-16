9da1a5195afad47ba5c5cde91f4eac20891f4867
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-04-16 14:21:59 +0000

    student to JSON

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python3
-"""This module creates a func that returns the dict for JSON serialization"""
+"""Defines a class named Student"""
 
 
-def class_to_json(obj):
-    """Returns the dictionary description of object
-    """
-    return obj.__dict__
+class Student:
+    """A class named Student"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes an instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves dictionary representation of student"""
+        if type(attrs) is list and all(type(x) is str for x in attrs):
+            dictionary = {}
+            for i in attrs:
+                if i in self.__dict__:
+                    dictionary[i] = self.__dict__[i]
+            return dictionary
+        else:
+            return self.__dict__
