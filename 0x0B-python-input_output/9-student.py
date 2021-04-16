b844949584717048f0bd45f766ebe561398cbdda
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-04-16 14:16:12 +0000

    student to JSON

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""This module creates a script that adds args to Python list and saves them"""
+"""Defines a class named Student"""
 
-from os import path
-from sys import argv
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-if path.exists("add_item.json") is False:
-    save_to_json_file([], "add_item.json")
-my_list = load_from_json_file("add_item.json")
-for i in range(1, len(argv)):
-    my_list.append(argv[i])
-save_to_json_file(my_list, "add_item.json")
+class Student:
+    """A class named Student"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes an instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Retrieves dictionary representation of student"""
+        return self.__dict__
