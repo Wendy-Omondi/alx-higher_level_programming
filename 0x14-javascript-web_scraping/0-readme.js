248d0aca1b2ac43317ffe52f7031b28bb4084426
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-05-04 15:48:12 +0000

    rewrite code without throw

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
+// Reads and prints the content of a file
 
 const fs = require('fs');
-
-fs.readFile(process.argv[2], 'utf-8', (err, data){
-if (err) throw err;
-console.log(data);
+fs.readFile(process.argv[2], 'utf8', function (error, data) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
 });
